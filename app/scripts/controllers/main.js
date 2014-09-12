e7c4902a24de202de55398ab547848525083df80
d0ln <273481701@qq.com>
2014-09-12 16:34:10 +0800

    d0ln

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name partyBidApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the partyBidApp
+ */
+angular.module('partyBidApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
